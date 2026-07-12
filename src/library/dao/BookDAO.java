1a53a3de95c30b1e74bddff153c4a9e8f51e343a
Rishika-Rajagiri <rajagiririshika@gmail.com>
2026-07-12 18:54:40 +0530

    BookDAO and MemberDAO are added

--- src/library/dao/BookDAO.java
+++ src/library/dao/BookDAO.java
@@ -1,4 +1,104 @@
 package library.dao;
 
+import library.db.DBConnection;
+import library.model.Book;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookDAO {
+    //CREATE - add a book
+    public void addBook(Book book) throws SQLException {
+        String sql = "INSERT INTO Book(title,author,category,total_copies,available_copies) VALUES(?,?,?,?,?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, book.getTitle());
+            ps.setString(2, book.getAuthor());
+            ps.setString(3, book.getCategory());
+            ps.setInt(4, book.getTotalCopies());
+            ps.setInt(5, book.getTotalCopies());
+            ps.executeUpdate();
+        }
+    }
+
+    //READ - get a book by ID
+    public Book getBookByID(int bookId) throws SQLException {
+        String sql = "SELECT * FROM Books WHERE book_id = ?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, bookId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Book(
+                            rs.getInt("book_id"),
+                            rs.getString("title"),
+                            rs.getString("author"),
+                            rs.getString("category"),
+                            rs.getInt("total_copies"),
+                            rs.getInt("available_copies")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    //READ - get all books
+    public List<Book> getAllBooks() throws SQLException{
+        List<Book> books=new ArrayList<>();
+        String sql="SELECT * FROM Books";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Book book = new Book(
+                        rs.getInt("book_id"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("category"),
+                        rs.getInt("total_copies"),
+                        rs.getInt("available_copies")
+                );
+                        books.add(book);
+            }
+        }
+        return books;
+    }
+
+    //UPDATE - update the existing book
+    public void updateBook(Book book) throws SQLException {
+        String sql = "UPDATE Books SET title = ?, author = ?, category = ?, total_copies = ?, available_copies = ? WHERE book_id = ?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, book.getTitle());
+            ps.setString(2, book.getAuthor());
+            ps.setString(3, book.getCategory());
+            ps.setInt(4, book.getTotalCopies());
+            ps.setInt(5, book.getAvailableCopies());
+            ps.setInt(6, book.getBookId());
+
+            ps.executeUpdate();
+        }
+    }
+
+    //DELETE -Remove a book
+    public void deleteBook(int bookId) throws SQLException {
+        String sql = "DELETE FROM Books WHERE book_id = ?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, bookId);
+            ps.executeUpdate();
+        }
+    }
+
 }
--- src/library/dao/MemberDAO.java
+++ src/library/dao/MemberDAO.java
@@ -1,4 +1,85 @@
 package library.dao;
+import library.db.DBConnection;
+import library.model.Member;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDAO {
-}
+    //CREATE - add a new member
+    public void addMember(Member member) throws SQLException{
+        String sql= "INSERT INTO Members (name, email, phone) VALUES (?, ?, ?)";
+        try(Connection con=DBConnection.getConnection();
+        PreparedStatement ps=con.prepareStatement(sql)){
+            ps.setString(1,member.getName());
+            ps.setString(2, member.getEmail());
+            ps.setString(3, member.getPhone());
+            ps.executeUpdate();
+        }
+    }
+
+    //READ - get a member by the id
+    public Member getMemberById(int memberId) throws SQLException {
+        String sql = "SELECT * FROM Members WHERE member_id = ?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, memberId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Member(
+                            rs.getInt("member_id"),
+                            rs.getString("name"),
+                            rs.getString("email"),
+                            rs.getString("phone"),
+                            rs.getDate("join_date").toLocalDate()
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    //READ - get all members
+    public List<Member> getAllMembers() throws SQLException{
+        List<Member> members=new ArrayList<>();
+        String sql="SELECT* FROM Members";
+        try(Connection con=DBConnection.getConnection();
+        PreparedStatement ps=con.prepareStatement(sql);
+        ResultSet rs=ps.executeQuery()){
+            while(rs.next()) {
+                members.add(new Member(
+                        rs.getInt("member_id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("phone"),
+                        rs.getDate("join_date").toLocalDate()
+                ));
+            }
+        }
+        return members;
+    }
+
+    //UPDATE - UPDATE AN EXISTING MEMBER
+    public void updateMember(Member member) throws SQLException {
+        String sql = "UPDATE Members SET name=?, email=?, phone=? WHERE member_id=?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, member.getName());
+            ps.setString(2, member.getEmail());
+            ps.setString(3, member.getPhone());
+            ps.setInt(4, member.getMemberId());
+            ps.executeUpdate();
+        }
+    }
+        //DELETE - delete a member
+       public void deleteMember(int memberId) throws SQLException {
+            String sql = "DELETE FROM Members WHERE member_id = ?";
+            try (Connection conn = DBConnection.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setInt(1, memberId);
+                stmt.executeUpdate();
+            }
+        }
+
+    }
+
